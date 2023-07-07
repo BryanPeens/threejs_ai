005560dca745c8dfa73c7084cd6d75d5ab39ba38
Bryan Peens <peensbryan1@gmail.com>
2023-07-07 00:14:46 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import dalleRoutes from './routes/dalle.routes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.listen(8080, () => console.log('Server has started on port 8080'));
